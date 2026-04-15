caa09332338ed8b23790bfd8089b86aca19950d0
Lugavra <felipe_gavranich@hotmail.com>
2026-04-15 20:51:33 -0300

    Update analise_exploratoria.sql

--- analise_exploratoria.sql
+++ analise_exploratoria.sql
@@ -22,3 +22,12 @@ SELECT
     END AS classificacao_abc
 FROM pareto_calc;
 
+SELECT 
+    customer_state,
+    AVG(DATE_DIFF(order_delivered_customer_date, order_purchase_timestamp, DAY)) AS media_dias_entrega,
+    COUNT(CASE WHEN order_delivered_customer_date > order_estimated_delivery_date THEN 1 END) * 100.0 / COUNT(*) AS percentual_atraso
+FROM olist_orders_dataset o
+JOIN olist_customers_dataset c ON o.customer_id = c.customer_id
+WHERE order_status = 'delivered'
+GROUP BY 1
+ORDER BY percentual_atraso DESC;
